39e5d789c0a401a81e19d2a10fefab7f790a6056
Shashank Kesarwani <am.shashank01@gmail.com>
2020-09-20 17:52:27 +0530

    sixth commit

---
+++ React/confusion/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color = "primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
